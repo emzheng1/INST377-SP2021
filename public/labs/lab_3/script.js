e22f09047b9b286f5045ee8d8f6b36967d3421be
Emily Zheng <69724738+emzheng1@users.noreply.github.com>
2021-02-17 19:47:12 -0500

    Fixed JS

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,19 @@
 /* Put your javascript in here */
+
+    let imgWidth = 130; 
+    let imgCount = 3;
+    let position = 0;
+    let list = carousel.querySelector('ul');
+    let listElems = carousel.querySelectorAll('li');
+      
+    carousel.querySelector('.prev').onclick = function() {
+      position += imgWidth * imgCount;
+      position = Math.min(position, 0)
+      list.style.marginLeft = position + 'px';
+    };
+
+    carousel.querySelector('.next').onclick = function() {
+      position -= imgWidth * imgCount;
+      position = Math.max(position, -imgWidth * (listElems.length - imgCount));
+      list.style.marginLeft = position + 'px';
+    };
